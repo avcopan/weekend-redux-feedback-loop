6e38b47c4c6c6ec3d7c6dc694e51931705f67834
Andreas Copan <avcopan@gmail.com>
2023-07-03 16:36:47 -0400

    Creates react routes.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { BrowserRouter, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
@@ -10,6 +11,20 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <BrowserRouter>
+        <Route exact path="/feedback/1">
+          <h2>How are you feeling today?</h2>
+        </Route> 
+        <Route exact path="/feedback/2">
+          <h2>How well are you understanding the content?</h2>
+        </Route> 
+        <Route exact path="/feedback/3">
+          <h2>How well are you being supported?</h2>
+        </Route> 
+        <Route exact path="/feedback/4">
+          <h2>Any comments you have?</h2>
+        </Route> 
+      </BrowserRouter>
     </div>
   );
 }
