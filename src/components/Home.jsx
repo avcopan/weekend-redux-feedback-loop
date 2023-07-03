fc957713c4d1b0812aae2fed007df1ccabff2589
Andreas Copan <avcopan@gmail.com>
2023-07-03 17:14:50 -0400

    Navigates to survey from home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
 export function Home() {
-  return <button>Start Survey</button>;
+  const navigate = useNavigate();
+
+  const handleOnClick = () => {
+    navigate("/survey/1");
+  };
+  return <button onClick={handleOnClick}>Start Survey</button>;
 }
