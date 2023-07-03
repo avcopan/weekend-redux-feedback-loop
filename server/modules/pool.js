1c06558bef833a6618b0f7a3f4e2d9fd835aa74a
Andreas Copan <avcopan@gmail.com>
2023-07-03 15:40:31 -0400

    Sets up pool for cross-platform usage

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,28 +1,44 @@
-const pg = require('pg');
+const pg = require("pg");
+const dotenv = require("dotenv");
+
+dotenv.config();
 let pool;
 
-// When our app is deployed to the internet 
+// When our app is deployed to the internet
 // we'll use the DATABASE_URL environment variable
 // to set the connection info: web address, username/password, db name
-// eg: 
+// eg:
 //  DATABASE_URL=postgresql://jDoe354:secretPw123@some.db.com/prime_app
 if (process.env.DATABASE_URL) {
-    pool = new pg.Pool({
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    });
+  pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
 }
 // When we're running this app on our own computer
-// we'll connect to the postgres database that is 
+// we'll connect to the postgres database that is
 // also running on our computer (localhost)
 else {
-    pool = new pg.Pool({
-        host: 'localhost',
-        port: 5432,
-        database: 'prime_feedback', 
-    });
+  pool_args = {
+    host: "localhost",
+    port: 5432,
+    database: "prime_feedback",
+  };
+  if (process.env.DB_USER) {
+    pool_args.user = process.env.DB_USER;
+  }
+  if (process.env.DB_PASSWORD) {
+    pool_args.password = process.env.DB_PASSWORD;
+  }
+  pool = new pg.Pool(pool_args);
 }
+
+pool
+  .query("SELECT * FROM feedback;")
+  .then((result) => result.rows)
+  .then(console.log)
+  .catch(console.error);
 
 module.exports = pool;
