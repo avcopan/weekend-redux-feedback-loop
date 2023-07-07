d10ee86a503c7b194345474f7a35741fb2533c8b
Andreas Copan <avcopan@gmail.com>
2023-07-07 15:30:48 -0400

    Closes #2
    Removes console.logs and other debugging code.

--- src/pages/SurveyQuestion.jsx
+++ src/pages/SurveyQuestion.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import surveySlice from "../state/surveySlice";
 
 const SURVEY = [
@@ -24,12 +24,10 @@ const SURVEY = [
 
 export function SurveyQuestion({ page }) {
   const dispatch = useDispatch();
-  const survey = useSelector((store) => store.survey);
   const navigate = useNavigate();
   const [inputValue, setInputValue] = useState("");
 
   const handleNextClick = () => {
-    console.log("HERE! DISPATCHING!");
     dispatch(SURVEY[page].action(inputValue));
     setInputValue("");
     navigate(`/survey/${page + 1}`);
@@ -45,8 +43,6 @@ export function SurveyQuestion({ page }) {
       <input type="text" value={inputValue} onChange={handleInputChange} />
       <br />
       <button onClick={handleNextClick}>Next</button>
-      <br />
-      {JSON.stringify(survey)}
     </>
   );
 }
--- src/state/surveySlice.js
+++ src/state/surveySlice.js
@@ -5,19 +5,15 @@ const surveySlice = createSlice({
   initialState: {},
   reducers: {
     setFeeling(state, action) {
-      console.log("in 1");
       return { ...state, feeling: action.payload };
     },
     setUnderstanding(state, action) {
-      console.log("in 2");
       return { ...state, understanding: action.payload };
     },
     setSupport(state, action) {
-      console.log("in 3");
       return { ...state, support: action.payload };
     },
     setComments(state, action) {
-      console.log("in 4");
       return { ...state, comments: action.payload };
     },
   },
