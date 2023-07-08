cca938550b75e1a7a7fb7c2b662369646bcf0163
Andreas Copan <avcopan@gmail.com>
2023-07-08 12:28:19 -0400

    Closes #15
    Allows user to navigate back and forth through survey, editing
    responses.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import "./Layout.css";
 
 export function Layout({ children }) {
+  const navigate = useNavigate();
+
+  const navigateHome = () => {
+    navigate("/");
+  };
+
   return (
     <div className="Layout">
       <header className="Layout-header">
-        <h1 className="Layout-title">Feedback!</h1>
+        <h1 className="Layout-title" onClick={navigateHome}>
+          Feedback!
+        </h1>
         <h4>Don't forget it!</h4>
       </header>
       {children}
--- src/pages/SurveyQuestion.jsx
+++ src/pages/SurveyQuestion.jsx
@@ -1,23 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import surveySlice from "../state/surveySlice";
 
 export function SurveyQuestion({ index }) {
+
   const survey = useSelector((store) => store.survey);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [response, setResponse] = useState("");
 
-  const handleClickNext = () => {
+  useEffect(() => {
+    setResponse(survey[index].response);
+  }, [index])
+
+  const saveResponse = () => {
     const payload = [index, response];
-    setResponse("");
-
-    // Enter the response to the store
     dispatch(surveySlice.actions.enterResponse(payload));
+  }
 
+  const navigatePrevious = () => {
+    saveResponse();
+    // Navigate to the previous question page, if there is one.
+    const route = index > 0 ? `/survey/${index - 1}` : "/";
+    navigate(route);
+  };
+
+  const navigateNext = () => {
+    saveResponse();
     // Navigate to the next question page, if there is one.
-    // Otherwise, navigate to the survey review page.
     const route =
       index + 1 < survey.length ? `/survey/${index + 1}` : "/survey/review";
     navigate(route);
@@ -32,7 +43,8 @@ export function SurveyQuestion({ index }) {
         onChange={(event) => setResponse(event.target.value)}
       />
       <br />
-      <button onClick={handleClickNext}>Next</button>
+      <button onClick={navigatePrevious}>Previous</button>
+      <button onClick={navigateNext}>Next</button>
     </>
   );
 }
