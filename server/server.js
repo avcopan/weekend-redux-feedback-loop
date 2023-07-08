8a90191b23cec23d8a9c8c2fdf0529e87e5e9cdd
Andreas Copan <avcopan@gmail.com>
2023-07-08 10:24:49 -0400

    Closes #10
    Implements basic GET/POST routes for the server.

--- server/server.js
+++ server/server.js
@@ -1,17 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const surveyRouter = require("./routes/survey.router.js");
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('build'));
+app.use(express.json());
+app.use(express.static("build"));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-
+app.use("/survey", surveyRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
-    console.log('Listening on port: ', PORT);
+  console.log("Listening on port: ", PORT);
 });
